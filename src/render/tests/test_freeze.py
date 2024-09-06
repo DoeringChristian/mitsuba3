a59e1c8e91f4c9ecaaa718e9c9543ce73626ca02
Christian Döring <christian.doering@tum.de>
2024-09-06 14:12:16 +0200

    Added trampoline traversal to bsdf and sampler and implemented tests

--- src/render/tests/test_freeze.py
+++ src/render/tests/test_freeze.py
@@ -271,7 +271,6 @@ def test03_optimize_color(variants_vec_rgb):
     # therefore we can test if the two methods produce the same results
     assert dr.allclose(param_ref, param_frozen)
     
-    
 @pytest.mark.parametrize(
     "bsdf",
     [
@@ -291,12 +290,84 @@ def test03_optimize_color(variants_vec_rgb):
         "twosided",
         "principled",
         "principledthin",
+        "custom",
     ],
 )
 def test04_bsdf(variants_vec_rgb, bsdf):
     # dr.set_log_level(dr.LogLevel.Trace)
     # dr.set_flag(dr.JitFlag.ReuseIndices, False)
     # dr.set_flag(dr.JitFlag.Debug, True)
+
+    if bsdf == "custom":
+        class MyBSDF(mi.BSDF):
+            def __init__(self, props):
+                mi.BSDF.__init__(self, props)
+
+                # Read 'eta' and 'tint' properties from `props`
+                self.eta = 1.33
+                if props.has_property('eta'):
+                    self.eta = props['eta']
+
+                self.tint = props['tint']
+
+                # Set the BSDF flags
+                reflection_flags   = mi.BSDFFlags.DeltaReflection   | mi.BSDFFlags.FrontSide | mi.BSDFFlags.BackSide
+                transmission_flags = mi.BSDFFlags.DeltaTransmission | mi.BSDFFlags.FrontSide | mi.BSDFFlags.BackSide
+                self.m_components  = [reflection_flags, transmission_flags]
+                self.m_flags = reflection_flags | transmission_flags
+
+            def sample(self, ctx, si, sample1, sample2, active):
+                # Compute Fresnel terms
+                cos_theta_i = mi.Frame3f.cos_theta(si.wi)
+                r_i, cos_theta_t, eta_it, eta_ti = mi.fresnel(cos_theta_i, self.eta)
+                t_i = dr.maximum(1.0 - r_i, 0.0)
+
+                # Pick between reflection and transmission
+                selected_r = (sample1 <= r_i) & active
+
+                # Fill up the BSDFSample struct
+                bs = mi.BSDFSample3f()
+                bs.pdf = dr.select(selected_r, r_i, t_i)
+                bs.sampled_component = dr.select(selected_r, mi.UInt32(0), mi.UInt32(1))
+                bs.sampled_type      = dr.select(selected_r, mi.UInt32(+mi.BSDFFlags.DeltaReflection),
+                                                             mi.UInt32(+mi.BSDFFlags.DeltaTransmission))
+                bs.wo = dr.select(selected_r,
+                                  mi.reflect(si.wi),
+                                  mi.refract(si.wi, cos_theta_t, eta_ti))
+                bs.eta = dr.select(selected_r, 1.0, eta_it)
+
+                # For reflection, tint based on the incident angle (more tint at grazing angle)
+                value_r = dr.lerp(mi.Color3f(self.tint), mi.Color3f(1.0), dr.clip(cos_theta_i, 0.0, 1.0))
+
+                # For transmission, radiance must be scaled to account for the solid angle compression
+                value_t = mi.Color3f(1.0) * dr.square(eta_ti)
+
+                value = dr.select(selected_r, value_r, value_t)
+
+                return (bs, value)
+
+            def eval(self, ctx, si, wo, active):
+                return 0.0
+
+            def pdf(self, ctx, si, wo, active):
+                return 0.0
+
+            def eval_pdf(self, ctx, si, wo, active):
+                return 0.0, 0.0
+
+            def traverse(self, callback):
+                callback.put_parameter('tint', self.tint, mi.ParamFlags.Differentiable)
+
+            def parameters_changed(self, keys):
+                print("🏝️ there is nothing to do here 🏝️")
+
+            def to_string(self):
+                return ('MyBSDF[\n'
+                        '    eta=%s,\n'
+                        '    tint=%s,\n'
+                        ']' % (self.eta, self.tint))
+
+        mi.register_bsdf("mybsdf", lambda props: MyBSDF(props))
     
     w = 16
     h = 16
@@ -376,6 +447,12 @@ def test04_bsdf(variants_vec_rgb, bsdf):
                     "type": "bitmap",
                     "filename": find_resource("resources/data/common/textures/wood.jpg"),
                 }
+            }
+        elif bsdf == "custom":
+            scene["white"] = {
+                "type": "mybsdf",
+                "tint": [0.2, 0.9, 0.2],
+                "eta": 1.33,
             }
         else:
             scene["white"] = {
@@ -620,7 +697,7 @@ def test06_integrators(variants_vec_rgb, integrator):
         return images
     
 
-    # scene = load_scene()
+    scene = load_scene()
     images_ref = run(n, func)
     images_frozen = run(n, dr.freeze(func))
     
@@ -635,7 +712,7 @@ def test06_integrators(variants_vec_rgb, integrator):
     "bsplinecurve",
     "linearcurve",
     "sdfgrid",
-    # "instance",
+    "instance",
     "sphere",
 ])
 def test07_shape(variants_vec_rgb, shape):
@@ -647,6 +724,7 @@ def test07_shape(variants_vec_rgb, shape):
     dr.set_flag(dr.JitFlag.ReuseIndices, False)
     dr.set_flag(dr.JitFlag.Debug, True)
     dr.set_flag(dr.JitFlag.LaunchBlocking, True)
+    dr.set_flag(dr.JitFlag.OptimizeCalls, False)
     
     def func(scene: mi.Scene) -> mi.TensorXf:
         with dr.profile_range("render"):
