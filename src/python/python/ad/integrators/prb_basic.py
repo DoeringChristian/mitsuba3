afe0fa425ee47dde88861ac0679b52934a017416
Christian Döring <christian.doering@tum.de>
2024-09-20 16:05:53 +0200

    Updated prb_basic integrator to work with new drjit version

--- src/python/python/ad/integrators/prb_basic.py
+++ src/python/python/ad/integrators/prb_basic.py
@@ -81,7 +81,8 @@ class BasicPRBIntegrator(RBIntegrator):
 
         while dr.hint(active,
                       max_iterations=self.max_depth,
-                      label="Path Replay Backpropagation (%s)" % mode.name):
+                      label="Path Replay Backpropagation (%s)" % mode.name, 
+                      exclude = [scene]):
             active_next = mi.Bool(active)
 
             # ---------------------- Direct emission ----------------------
@@ -162,7 +163,11 @@ class BasicPRBIntegrator(RBIntegrator):
 
         return (
             L if primal else δL, # Radiance/differential radiance
+<<<<<<< HEAD
             depth != 0,          # Ray validity flag for alpha blending
+=======
+            depth != 0,    # Ray validity flag for alpha blending
+>>>>>>> 0b71f8e7 (Updated prb_basic integrator to work with new drjit version)
             [],                  # Empty typle of AOVs
             L                    # State the for differential phase
         )
